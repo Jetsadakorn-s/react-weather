cac63aab1565fa8e2ab0ac78234fbcf8957187b0
Jetsadakorn <my1471@hotmail.com>
2023-02-09 12:17:40 +0700

    finish
    fix bug from can't render when fetch API

--- src/App.js
+++ src/App.js
@@ -5,36 +5,46 @@ function App() {
   const name = "Bangkok"
   const apikey="cf649fdf91c4eab7c28487ed5c016f3e"
   const [city,setCity] = useState({})
+  const [isLoading,setIsLoading] = useState(false)
+  
+  const fetchWeather = async () =>{
+    await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apikey}`)
+    .then((res) => res.json())
+    .then((data) =>{
+      setCity(data)
+      setIsLoading(true)}  
+  )}
 
   useEffect(()=>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apikey}`
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>{
-      setCity(data)
-    })
-  },[])
-  return (
-    <div className='App'>
-      <section>
-        <div className='location'>
-          <h1 className='city'>Bangkok</h1>
-          <p className='state'>TH</p>
-        </div>
-        <div className='card'>
-          <div className='weather'>
-            <h1>28</h1>
-            <small>max:37, min:20</small>
-          </div>
-          <div className='info'>
-            <div className='status'>อากาศดี</div>
-            <div className='humidity'>100</div>
-            <div className='wind'>4.0</div>
-          </div>
-        </div>
-      </section>
-    </div>
+    fetchWeather();
+    }
+  ,[name])
+  
 
+  const convertTemp=(k)=>{
+    return (k-273).toFixed(2)
+  }
+  return ( 
+    (isLoading && 
+    <div className='App'>
+    <section>
+      <div className='location'>
+        <h1 className='city'>{city.name}</h1>
+        <p className='state'>{city.sys.country}</p>
+      </div>
+      <div className='card'>
+        <div className='weather'>
+          <h1>{convertTemp(city.main.temp)}&deg;C</h1>
+          <small>max : {convertTemp(city.main.temp_max)}&deg;C, min : {convertTemp(city.main.temp_min)}&deg;C</small>
+        </div>
+        <div className='info'>
+          <div className='status'>{city.weather[0].main}</div>
+          <div className='humidity'>ความชื้น = {city.main.humidity}</div>
+          <div className='wind'>ความเร็วลม = {city.wind.speed}</div>
+        </div>
+      </div>
+    </section>
+  </div>)
   );
 }
 
